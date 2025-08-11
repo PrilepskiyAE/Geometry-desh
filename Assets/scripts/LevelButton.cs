1320805cc120f6d98ff77226c174313bd27041d9
Alex92w <woolf7754286@yandex.ru>
2025-08-11 23:34:42 +0700

    dev
    dev

--- Assets/scripts/LevelButton.cs
+++ Assets/scripts/LevelButton.cs
@@ -1,11 +1,19 @@
+using TMPro;
 using UnityEngine;
 
 public class LevelButton : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private NavigationSystem _nav;
+    [SerializeField] private string _sname;
+    [SerializeField] private TextMeshProUGUI _uGUI;
     void Start()
     {
-        
+        if (PlayerPrefs.HasKey(_sname))
+        {
+            float persent = PlayerPrefs.GetFloat(_sname);
+            _uGUI.text = persent + "%";
+        }
+        else _uGUI.text = "0%";
     }
 
     // Update is called once per frame
